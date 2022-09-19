3923bdb08d91fb683714993efbe60b6dcde7897c
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-09-19 16:02:53 +0700

    bô cuc

---
+++ src/app/page/page-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { CoursesComponent } from './courses/courses.component';
+import { DetailBlogComponent } from './detail-blog/detail-blog.component';
+import { DetailCoursesComponent } from './detail-courses/detail-courses.component';
+import { HomeComponent } from './home/home.component';
+import { LearningComponent } from './learning/learning.component';
+import { PageComponent } from './page.component';
+
+const routes: Routes = [
+  {
+    path: '', component: PageComponent, children: [
+      { path: '', component: HomeComponent },
+      { path: 'blogs', component: BlogComponent },
+      { path: 'blog', component: DetailBlogComponent },
+      { path: 'courses', component: CoursesComponent },
+      { path: 'learning', component: LearningComponent },
+      { path: 'course', component: DetailCoursesComponent },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PageRoutingModule { }
