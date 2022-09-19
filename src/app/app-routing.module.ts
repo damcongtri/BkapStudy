00caedf4b21ef47a7a5a1b30c430388012b0f412
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2022-09-19 13:59:52 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './page/blog/blog.component';
+import { CoursesComponent } from './page/courses/courses.component';
+import { DetailBlogComponent } from './page/detail-blog/detail-blog.component';
+import { HomeComponent } from './page/home/home.component';
+import { LearningComponent } from './page/learning/learning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'blogs', component: BlogComponent },
+  { path: 'blog', component: DetailBlogComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'learning', component: LearningComponent },
+  { path: 'course', component: DetailBlogComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './page/header/header.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { BlogComponent } from './page/blog/blog.component';
+import { DetailBlogComponent } from './page/detail-blog/detail-blog.component';
+import { LearningComponent } from './page/learning/learning.component';
+import { CoursesComponent } from './page/courses/courses.component';
+import { DetailCoursesComponent } from './page/detail-courses/detail-courses.component';
+import { MaterialComponent } from './page/learning/material/material.component';
+import { NoteComponent } from './page/learning/note/note.component';
+import { QAComponent } from './page/learning/q-a/q-a.component';
+import { ExerciseComponent } from './page/learning/exercise/exercise.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogComponent,
+    DetailBlogComponent,
+    LearningComponent,
+    CoursesComponent,
+    DetailCoursesComponent,
+    MaterialComponent,
+    NoteComponent,
+    QAComponent,
+    ExerciseComponent
   ],
   imports: [
     BrowserModule,
