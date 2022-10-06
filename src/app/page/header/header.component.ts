1a054340c490d7721dfde31893fa5f57457259b4
Minh <minhka192@gmail.com>
2022-10-06 17:17:56 +0700

    minh

--- src/app/page/header/header.component.ts
+++ src/app/page/header/header.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+ 
   constructor() { }
+  user:any
 
   ngOnInit(): void {
+    this.user =  localStorage.getItem('acc') ? JSON.parse(localStorage.getItem('acc') as string ) : null;
+
+  }
+  Logout(){
+      localStorage.removeItem('acc');
+      this.user =  localStorage.getItem('acc') ? JSON.parse(localStorage.getItem('acc') as string ) : null;
+      
   }
 
 }
